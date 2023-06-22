9d4f969a773ad5757ecded74799a65188b844801
sanin <saninsnz@gmail.com>
2023-06-22 06:16:38 +0530

    clean code

--- lib/ui/home_screen/home_screen.dart
+++ lib/ui/home_screen/home_screen.dart
@@ -1,31 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:video_player/video_player.dart';
-// import 'package:chewie/chewie.dart';
-//
-// class HomeScreen extends StatefulWidget {
-//   @override
-//   _HomeScreenState createState() => _HomeScreenState();
-// }
-//
-// class _HomeScreenState extends State<HomeScreen> {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Online Video Player'),
-//       ),
-//       body: Center(
-//         child: Chewie(
-//           controller: _chewieController!,
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
